b998cf649f67ac5bd786547b983390f0c6c939e0
bakid <josephbawo@gmail.com>
2022-12-06 01:42:50 +0100

    tried to code a nav bar

--- mode.js
+++ mode.js
@@ -1,24 +1,29 @@
-const colorSwitch = document.getElementById('input-color-switch');
+const colorSwitch = document.getElementById("input-color-switch");
 
-colorSwitch.addEventListener('click' ,checkMode);
+colorSwitch.addEventListener("click", checkMode);
 
-function checkMode(){
-    console.log('checking...');
-    if(colorSwitch.checked){
-        console.log('dark on');
-        darkModeOn();
-    }
-        else{
-            console.log('dark off');
-            darkModeOff();
-        }
-    }
+function checkMode() {
+  console.log("checking...");
+  if (colorSwitch.checked) {
+    console.log("dark on");
+    darkModeOn();
+  } else {
+    console.log("dark off");
+    darkModeOff();
+  }
+}
 
+function darkModeOn() {
+  document.body.classList.add("light-mode");
+}
 
-    function darkModeOn(){
-        document.body.classList.add("light-mode");
-    }
+function darkModeOff() {
+  document.body.classList.remove("light-mode");
+}
 
-    function darkModeOff(){
-         document.body.classList.remove('light-mode')
-    }
+const hamburger = document.getElementById("hamburger");
+const UL = document.getElementById("ul");
+
+hamburger.addEventListener("click", () => {
+  UL.classList.toggle("show");
+});
