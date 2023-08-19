462e5f87e926aa0e62ca2c395370a82174cea095
joebakid <josephbawo@gmail.com>
2023-08-19 20:23:13 +0100

    deleting un-used codes

--- mode.js
+++ mode.js
@@ -1,25 +1,3 @@
-// const colorSwitch = document.getElementById("input-color-switch");
-
-// colorSwitch.addEventListener("click", checkMode);
-
-// function checkMode() {
-//   console.log("checking...");
-//   if (colorSwitch.checked) {
-//     // console.log("dark on");
-//     darkModeOn();
-//   } else {
-//     // console.log("dark off");
-//     darkModeOff();
-//   }
-// }
-
-// function darkModeOn() {
-//   document.body.classList.add("light-mode");
-// }
-
-// function darkModeOff() {
-//   document.body.classList.remove("light-mode");
-// }
 const colorSwitch = document.getElementById("input-color-switch");
 
 // Check if user has a preference saved in localStorage
